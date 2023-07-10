47401e7c1eaba0a97722108d34f14352ded69d00
proshantosaha <proshantoll71@gmail.com>
2023-07-10 09:22:40 +0600

    implement in next js json data

---
+++ components/user.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function User({dser}) {
+  return (
+    <>
+    <p>{User.name}</p>
+    <p>{User.email}</p>
+    
+    
+    </>
+  )
+}
---
+++ pages/users.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import User from '@/components/user';
+
+export default function UserList({data}) {
+  return (
+    <>
+        <h1>UserList</h1>
+        {data.map((user)=>{
+                return(
+                    <div key={user.id}>
+                        <p>{user.name}</p>
+                      <User user={user}/>
+                    </div>
+                )
+            })}
+    </>
+  )
+}
+
+
+export async function getStaticProps(){
+
+    const response = await fetch('http://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+
+    return{
+        props:{
+            data:data
+
+        }
+    }
+}
